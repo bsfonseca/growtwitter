685c71c0f91bd941aa9893ddec7f26a58290a9a2
bsfonseca <bs.fonseca12@gmail.com>
2023-10-08 18:24:31 -0300

    #5 #7 - Criação e exibição dos likes nos tweets

--- src/models/tweet.ts
+++ src/models/tweet.ts
@@ -5,27 +5,39 @@ export class Tweet {
     public id: string;
     public replies: Tweet[];
     public user: User;
+    public likes: User[];
 
     constructor(public content: string, public type: string, user: User) {
         this.id = v4();
         this.replies = [];
         this.user = user;
+        this.likes = [];
     }
-
+    //Resposta
     reply(conteudo: string, user: User) {
         const reply = new Tweet(conteudo, "R", user);
         this.replies.push(reply);
     }
 
-    like() {
-        console.log("like");
+    //Curtir
+    like(usuario: User) {
+        this.likes.push(usuario);
     }
 
+    //Mostrar
     show(username: string) {
         console.log(`@${username}: ${this.content}`);
 
         for (let item of this.replies) {
             console.log(` > @${item.user.username}: ${item.content}`);
+        }
+
+        if (this.likes.length == 1) {
+            console.log(`[@${this.likes[0].username} curtiu]`);
+        } else if (this.likes.length == 2) {
+            console.log(`[@${this.likes[0].username} e mais ${this.likes.length - 1} usuário curtiu]`);
+        } else if (this.likes.length > 2) {
+            console.log(`[@${this.likes[0].username} e mais ${this.likes.length - 1} usuários curtiram]`);
         }
     }
 
