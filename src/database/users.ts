9dcaa845a709a532fb91d82f0018cd9e77fe2012
bsfonseca <bs.fonseca12@gmail.com>
2023-10-08 18:27:09 -0300

    Testes das features da aplicação

--- src/database/users.ts
+++ src/database/users.ts
@@ -1,6 +1,8 @@
 import { User } from "../models/user";
 
+// Lista de usuários
 const daphne = new User("Daphne", "daphne", "daphne@dog.com", "13548");
-const bruna = new User("Bruna", "bruna", "bruna@cat.com", "13548");
+const bruna = new User("Bruna", "bruna", "bruna@.com", "13548");
+const renata = new User("Renata", "renata", "renata@gmail.com", "10152");
 
-export const users: User[] = [daphne, bruna];
+export const users: User[] = [daphne, bruna, renata];
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,69 @@
 import { users } from "./database/users";
 
+// ------------- USUÁRIOS --------------
+
 const daphne = users[0];
+const bruna = users[1];
+const renata = users[2];
+
+// --------- CRIAÇÃO DE TWEETS ---------
+
 daphne.sendTweet("Ola");
+
+// Criou mais um tweet para criar um reply
 const tweetPracinha = daphne.sendTweet("Vou para pracinha");
 
 // O sendTweet retorna a referencia do tweet criado
 const tweetPassear = daphne.sendTweet("Vou passear");
 
-const bruna = users[1];
-bruna.sendTweet("A comida esta pronta");
+//Criou um tweet
+bruna.sendTweet("A comida esta pronta!");
+
+//Criou um tweet para reply
+const tweetChimarrao = bruna.sendTweet("Vamos tomar chimarrão, @renata? ");
+
+renata.sendTweet("Hoje o dia está legal para tomar um sorvete!!!");
+
+const tweetSorvete = renata.sendTweet("Alguém quer sair para tomar um sorvete?");
+
+// ---------- SEGUIDORES ---------------
 
 daphne.follow(bruna);
+renata.follow(bruna);
+daphne.follow(renata);
+bruna.follow(daphne);
+renata.follow(daphne);
 
-daphne.reply("Obaaa!!!", tweetPassear);
+// ----------- REPLIES -----------------
+
+daphne.reply("Obaaa!", tweetPassear);
 bruna.reply("Vou com vc", tweetPracinha);
+renata.reply("Vamos quero!!", tweetChimarrao);
+bruna.reply("Eu quero!!!", tweetSorvete);
+
+// ------------- LIKES ------------------
+
+tweetChimarrao.like(daphne);
+tweetChimarrao.like(renata);
+tweetChimarrao.like(bruna);
+
+// ------------- SHOW TWEETS -----------
 
 daphne.showTweets();
+console.log("\n-------------\n");
+
+bruna.showTweets();
+console.log("\n--------------\n");
+
+renata.showTweets();
+console.log("\n--------------\n");
+
+renata.showTweets();
+console.log("\n--------------\n");
+
+// ------------- SHOW FEED -------------
+
+bruna.showFeed();
+console.log("\n--------------\n");
+
+renata.showFeed();
